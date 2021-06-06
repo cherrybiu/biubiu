2902bb07caff7cc9cc64d8746c63c5fad4ba005a
淡航 <m15274906257@163.com>
2021-06-06 22:18:44 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+module.exports = new Map([
+    ['CMS-SaaS业务系统地址文档', {url: 'https://supermonkey.feishu.cn/docs/doccniKXbHJw3JMLaDQBxA6dRqb', type: 'saas'}],
+    ['CMS-SaaS文档库', {url: 'http://saas.doc.internal.supermonkey.com.cn/', type: 'saas'}],
+    ['超级猩猩在线文档', {url: 'http://fe.internal.supermonkey.com.cn/#/'}],
+    ['项目git仓库', {url: 'http://git.internal.supermonkey.com.cn:10080/supermonkey-web'}],
+    ['本周例会纪要', {url: 'https://supermonkey.feishu.cn/docs/doccnVgyvE5jdNPh6GlTSlWnPXe'}],
+    ['nei接口文档地址', {url: 'http://nei.internal.supermonkey.com.cn/progroup/home/management/'}],
+    ['ones平台', {url: 'https://ones.ai/project/#/home/project/view/154XmdRM'}],
+    ['dev环境获取验证码', {url: 'https://user.fat.supermonkey.com.cn/admin/user/getCaptcha?phoneNumber=', type: 'app'}]
+]);
